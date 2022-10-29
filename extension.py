fbe0f777cd65d3cd9eb7c56281d03b8ff7d5b397
ALOKUMAR0809 <115224029+ALOKUMAR0809@users.noreply.github.com>
2022-10-29 16:12:52 +0530

    Add files via upload

---
+++ extension.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 29 14:24:55 2022
+
+@author: Alok Kumar
+"""
+
+fn= input("Enter Filename: ")
+
+f = fn.split(".")
+
+print ("Extension of the file is : " + f[-1])
---
+++ radius.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 29 14:11:05 2022
+
+@author: Alok Kumar
+"""
+
+from math import pi
+
+r = float(input ("Enter radius of circle : "))
+
+print ("Area of the circle is " + str(pi * r**2))
